19a685487844ebda3f7d6f071d1ef1d7c0881eb1
Greg Bateman <gerbot150@gmail.com>
2017-04-05 00:01:36 -0400

    Almost functional

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const uuid = require('uuid/v4');
+const exec = require('child_process').exec;
+const fs = require('fs');
 const app = express();
-
-let downloadCount = 0;
 
 app.set('port', process.env.PORT || 5000);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
@@ -17,11 +19,34 @@ app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
   const id = uuid();
-  response.render('index', {id: id});
+  response.render('index', {
+    id: id
+  });
 });
 
-app.get('/download/:id', function(request, response) {
-  response.render('download', {id: request.params.id});
+app.post('/download/:id', function(request, response) {
+  exec('mkdir download/' + request.params.id, function(error, stdout, stderr) {
+    const path = 'download/' + request.params.id + '/';
+    if (!error) {
+      exec('echo "' + request.body.text_area + '" >> ' + path + 'Object.json',
+        function(error, stdout, stderr) {
+          if (!error) {
+            exec('swift/SwiftyFire/SwiftyFire < ' + path + 'Object.json > ' + path + 'Object.swift',
+              function(error, stdout, stderr) {
+                if (!error) {
+                  const file = fs.createReadStream(path + 'Object.swift');
+                  const download = fs.createWriteStream('Object.swift');
+                  file.pipe(download);
+                }
+              });
+            // response.render('download', {
+            //   id: request.params.id
+            // });
+          }
+        });
+    }
+  });
+  // response.sendStatus(404);
 });
 
 app.listen(app.get('port'), function() {
