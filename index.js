2224fee2a3c7c535d48fced49db0fb833307c2d5
Greg Bateman <gerbot150@gmail.com>
2017-04-23 20:44:38 +0200

    Added top level object name field

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 
 const systemPrefix = process.env.NODE_ENV === 'production' ? 'linux-' : '';
 
-
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded({
@@ -28,22 +27,27 @@ app.get('/', function(request, response) {
 });
 
 app.post('/download/:id', function(request, response) {
+  const text = request.body.text_area;
+  const name = request.body.name || 'Object';
   exec('mkdir -p download/' + request.params.id, function(error, stdout, stderr) {
     const path = 'download/' + request.params.id + '/';
     if (!error) {
-      exec('echo \'' + request.body.text_area + '\' >> ' + path + 'Object.json',
+      exec('echo \'' + text + '\' >> ' + path + name + '.json',
         function(error, stdout, stderr) {
           if (!error) {
-            exec('swift/SwiftyFire/' + systemPrefix + 'SwiftyFire < ' +
-              path + 'Object.json > ' + path + 'Object.swift', {
+            exec('swift/SwiftyFire/' + systemPrefix + 'SwiftyFire ' +
+              path + name + '.json > ' + path + name + '.swift', {
                 env: {
                   'LD_LIBRARY_PATH': __dirname + '/swift/SwiftyFire/linux-libs:/usr/local/lib64/:$LD_LIBRARY_PATH'
                 }
               },
               function(error, stdout, stderr) {
                 if (!error) {
-                  response.download(path + 'Object.swift', 'Object.swift', function (error) {
+                  response.download(path + name + '.swift', name + '.swift', function(error) {
                     exec('rm -rf ' + path, function(error, stdout, stderr) {});
+                    if (error) {
+                      console.log(error);
+                    }
                   });
                 } else {
                   console.log(error);
