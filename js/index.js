047de35200c464ae0899c5280e4ff0038a4fe6d3
Greg Bateman <gerbot150@gmail.com>
2017-12-20 21:21:52 -0500

    Created initial node.js files, using express and pug

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/', function(request, response)) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('App is running on port: ', app.get('port'));
+});
